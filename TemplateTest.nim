8095c7d620c680ea008daa5b5982b28e53398761
Unknown <francesco.cameli95@gmail.com>
2019-07-22 15:29:55 +0100

    First commit: inputs and outputs macros

---
+++ TemplateTest.nim
@@ -0,0 +1,30 @@
+from macros import expandMacros
+import macrosDSP
+    
+#inputs 2, "freq", "phase"
+
+#[ inputs 2:
+    "freq"
+    "phase"
+
+inputs:
+    2
+    "freq"
+    "phase" ]#
+
+#expandMacros(inputs(2, "freq", "phase"))
+
+#expandMacros(inputs(2))
+
+inputs 2:
+    "freq"
+    "phase"
+
+outputs 1:
+    "audio"
+
+echo ugen_inputs
+echo ugen_input_names
+
+echo ugen_outputs
+echo ugen_output_names
---
+++ macrosDSP.nim
@@ -0,0 +1,279 @@
+import macros
+
+#The block form (derived from using num_of_inputs as int literal, and param_names as a code block.):
+#inputs 1:
+#   "freq"
+macro inputs*(num_of_inputs : untyped, param_names : untyped) : untyped =
+    
+    var 
+        num_of_inputs_VAL : int
+        param_names_array_node : NimNode = nnkBracket.newTree()
+
+    #Must be an int literal
+    if num_of_inputs.kind != nnkIntLit: #Just as the expectKind proc
+        error("Expected the number of inputs to be expressed by an integer literal value")
+
+    if param_names.kind != nnkStmtList:
+        error("Expected a block statement after the number of inputs")
+    
+    num_of_inputs_VAL = int(num_of_inputs.intVal)     #Actual value of the int literal
+
+    if num_of_inputs_VAL < 0:
+        error("Expected a positive number for inputs number")
+
+    if num_of_inputs_VAL > 32:
+        error("Exceeded maximum number of inputs, 32")
+
+    var 
+        statement_counter = 0
+
+    for statement in param_names.children():
+        if statement.kind != nnkStrLit:
+            error("Expected parameter name number " & $(statement_counter + 1) & " to be a string literal value")
+        
+        param_names_array_node.add newLit(statement.strVal())
+        statement_counter += 1
+    
+    if statement_counter != num_of_inputs_VAL:
+        error("Expected " & $num_of_inputs_VAL & " param names, got " & $statement_counter)
+
+    result = quote do: 
+        let ugen_inputs {.inject.} = `num_of_inputs_VAL` #{.inject.} acts just like Julia's esc(). backticks to insert variable from macro's scope
+        let ugen_input_names {.inject.} = `param_names_array_node`  #It's possible to insert NimNodes directly in the code block 
+
+macro inputs*(num_of_inputs : untyped, param_names : varargs[untyped]) : untyped = 
+    
+    var 
+        num_of_inputs_VAL : int
+        param_names_array_node : NimNode = nnkBracket.newTree()
+
+    #The other block form (derived from num_of_inputs being a block of code)
+    #inputs: 
+    #   1
+    #   "freq"
+    if num_of_inputs.kind == nnkStmtList:
+        
+        var 
+            statement_counter = 0
+            param_names_counter = 0
+
+        for statement in num_of_inputs.children():
+            if statement_counter == 0:
+                if statement.kind != nnkIntLit:
+                    error("Expected the number of inputs to be expressed by an integer literal value")
+                
+                num_of_inputs_VAL = int(statement.intVal)
+                
+                if num_of_inputs_VAL < 0:
+                    error("Expected a positive number for inputs number")
+        
+                if num_of_inputs_VAL > 32:
+                    error("Exceeded maximum number of inputs, 32")
+            else:
+                if statement.kind != nnkStrLit:
+                    error("Expected parameter name number " & $statement_counter & " to be a string literal value")
+                
+                param_names_array_node.add newLit(statement.strVal())
+                param_names_counter += 1
+
+            statement_counter += 1
+
+        if param_names_counter > 0:
+            if param_names_counter != num_of_inputs_VAL:
+                error("Expected " & $num_of_inputs_VAL & " param names, got " & $param_names_counter)
+
+            result = quote do: 
+                let ugen_inputs {.inject.} = `num_of_inputs_VAL` #{.inject.} acts just like Julia's esc(). backticks to insert variable from macro's scope
+                let ugen_input_names {.inject.} = `param_names_array_node`  #It's possible to insert NimNodes directly in the code block
+        else:
+            result = quote do:
+                let ugen_inputs {.inject.} = `num_of_inputs_VAL`  
+                let ugen_input_names {.inject.} = ["NO_PARAM_NAMES"]
+
+    #The standard form (derived by using num_of_inputs as int literal, and successive param_names as varargs[untyped]):
+    #inputs 1, "freq"  OR inputs(1, "freq")
+    else:
+        #Must be an int literal
+        if num_of_inputs.kind != nnkIntLit: #Just as the expectKind proc
+            error("Expected the number of inputs to be expressed by an integer literal value")
+        
+        num_of_inputs_VAL = int(num_of_inputs.intVal)     #Actual value of the int literal
+
+        if num_of_inputs_VAL < 0:
+            error("Expected a positive number for inputs number")
+
+        if num_of_inputs_VAL > 32:
+            error("Exceeded maximum number of inputs, 32")
+        
+        #Empty bracket statement: []
+        param_names_array_node = nnkBracket.newTree()
+        
+        #Check for correct length of param names
+        if len(param_names) > 0:
+            if len(param_names) != num_of_inputs_VAL:
+                error("Expected " & $num_of_inputs_VAL & " param names, got " & $(len(param_names)))
+            
+            #Check if all param names are string literal values
+            for index, param_name in param_names:
+                if param_name.kind != nnkStrLit:
+                    error("Expected parameter name number " & $(index + 1) & " to be a string literal value")
+                
+                #Add literal string value to the nnkBracket NimNode
+                param_names_array_node.add newLit(param_name.strVal())
+            
+            #[ 
+                param_names_array_node will now be in the form:
+                nnkBracket.newTree(
+                    newLit("freq"),
+                    newLit("phase")
+                ) 
+                Which is a bracket statement, like: ["freq", "phase"]
+            ]#
+            
+            #Actual return statement: a valid NimNode wrapped in the "quote do:" syntax.
+            result = quote do: 
+                let ugen_inputs {.inject.} = `num_of_inputs_VAL` #{.inject.} acts just like Julia's esc(). backticks to insert variable from macro's scope
+                let ugen_input_names {.inject.} = `param_names_array_node`  #It's possible to insert NimNodes directly in the code block
+        else:
+            result = quote do:
+                let ugen_inputs {.inject.} = `num_of_inputs_VAL` 
+                let ugen_input_names {.inject.} = ["NO_PARAM_NAMES"]  
+        
+#The block form (derived from using num_of_outputs as int literal, and param_names as a code block.):
+#outputs 1:
+#   "freq"
+macro outputs*(num_of_outputs : untyped, param_names : untyped) : untyped =
+    
+    var 
+        num_of_outputs_VAL : int
+        param_names_array_node : NimNode = nnkBracket.newTree()
+
+    #Must be an int literal
+    if num_of_outputs.kind != nnkIntLit: #Just as the expectKind proc
+        error("Expected the number of outputs to be expressed by an integer literal value")
+
+    if param_names.kind != nnkStmtList:
+        error("Expected a block statement after the number of outputs")
+    
+    num_of_outputs_VAL = int(num_of_outputs.intVal)     #Actual value of the int literal
+
+    if num_of_outputs_VAL < 0:
+        error("Expected a positive number for outputs number")
+
+    if num_of_outputs_VAL > 32:
+        error("Exceeded maximum number of outputs, 32")
+
+    var 
+        statement_counter = 0
+
+    for statement in param_names.children():
+        if statement.kind != nnkStrLit:
+            error("Expected parameter name number " & $(statement_counter + 1) & " to be a string literal value")
+        
+        param_names_array_node.add newLit(statement.strVal())
+        statement_counter += 1
+    
+    if statement_counter != num_of_outputs_VAL:
+        error("Expected " & $num_of_outputs_VAL & " param names, got " & $statement_counter)
+
+    result = quote do: 
+        let ugen_outputs {.inject.} = `num_of_outputs_VAL` #{.inject.} acts just like Julia's esc(). backticks to insert variable from macro's scope
+        let ugen_output_names {.inject.} = `param_names_array_node`  #It's possible to insert NimNodes directly in the code block 
+
+macro outputs*(num_of_outputs : untyped, param_names : varargs[untyped]) : untyped = 
+    
+    var 
+        num_of_outputs_VAL : int
+        param_names_array_node : NimNode = nnkBracket.newTree()
+
+    #The other block form (derived from num_of_outputs being a block of code)
+    #outputs: 
+    #   1
+    #   "freq"
+    if num_of_outputs.kind == nnkStmtList:
+        
+        var 
+            statement_counter = 0
+            param_names_counter = 0
+
+        for statement in num_of_outputs.children():
+            if statement_counter == 0:
+                if statement.kind != nnkIntLit:
+                    error("Expected the number of outputs to be expressed by an integer literal value")
+                
+                num_of_outputs_VAL = int(statement.intVal)
+                
+                if num_of_outputs_VAL < 0:
+                    error("Expected a positive number for outputs number")
+        
+                if num_of_outputs_VAL > 32:
+                    error("Exceeded maximum number of outputs, 32")
+            else:
+                if statement.kind != nnkStrLit:
+                    error("Expected parameter name number " & $statement_counter & " to be a string literal value")
+                
+                param_names_array_node.add newLit(statement.strVal())
+                param_names_counter += 1
+
+            statement_counter += 1
+
+        if param_names_counter > 0:
+            if param_names_counter != num_of_outputs_VAL:
+                error("Expected " & $num_of_outputs_VAL & " param names, got " & $param_names_counter)
+
+            result = quote do: 
+                let ugen_outputs {.inject.} = `num_of_outputs_VAL` #{.inject.} acts just like Julia's esc(). backticks to insert variable from macro's scope
+                let ugen_output_names {.inject.} = `param_names_array_node`  #It's possible to insert NimNodes directly in the code block
+        else:
+            result = quote do:
+                let ugen_outputs {.inject.} = `num_of_outputs_VAL`  
+                let ugen_output_names {.inject.} = ["NO_PARAM_NAMES"]
+
+    #The standard form (derived by using num_of_outputs as int literal, and successive param_names as varargs[untyped]):
+    #outputs 1, "freq"  OR outputs(1, "freq")
+    else:
+        #Must be an int literal
+        if num_of_outputs.kind != nnkIntLit: #Just as the expectKind proc
+            error("Expected the number of outputs to be expressed by an integer literal value")
+        
+        num_of_outputs_VAL = int(num_of_outputs.intVal)     #Actual value of the int literal
+
+        if num_of_outputs_VAL < 0:
+            error("Expected a positive number for outputs number")
+
+        if num_of_outputs_VAL > 32:
+            error("Exceeded maximum number of outputs, 32")
+        
+        #Empty bracket statement: []
+        param_names_array_node = nnkBracket.newTree()
+        
+        #Check for correct length of param names
+        if len(param_names) > 0:
+            if len(param_names) != num_of_outputs_VAL:
+                error("Expected " & $num_of_outputs_VAL & " param names, got " & $(len(param_names)))
+            
+            #Check if all param names are string literal values
+            for index, param_name in param_names:
+                if param_name.kind != nnkStrLit:
+                    error("Expected parameter name number " & $(index + 1) & " to be a string literal value")
+                
+                #Add literal string value to the nnkBracket NimNode
+                param_names_array_node.add newLit(param_name.strVal())
+            
+            #[ 
+                param_names_array_node will now be in the form:
+                nnkBracket.newTree(
+                    newLit("freq"),
+                    newLit("phase")
+                ) 
+                Which is a bracket statement, like: ["freq", "phase"]
+            ]#
+            
+            #Actual return statement: a valid NimNode wrapped in the "quote do:" syntax.
+            result = quote do: 
+                let ugen_outputs {.inject.} = `num_of_outputs_VAL` #{.inject.} acts just like Julia's esc(). backticks to insert variable from macro's scope
+                let ugen_output_names {.inject.} = `param_names_array_node`  #It's possible to insert NimNodes directly in the code block
+        else:
+            result = quote do:
+                let ugen_outputs {.inject.} = `num_of_outputs_VAL` 
+                let ugen_output_names {.inject.} = ["NO_PARAM_NAMES"]  
