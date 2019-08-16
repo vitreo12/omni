cf6754aa3be5414add8b31f945cefe88e4539096
Unknown <francesco.cameli95@gmail.com>
2019-08-16 16:52:58 +0100

    Modified compile time check for d:supercollider symbol to use or not rt_alloc from C or standard Nim's alloc

--- RTAllocTest/rt_alloc.nim
+++ RTAllocTest/rt_alloc.nim
@@ -9,14 +9,41 @@ proc init_world*(in_world : pointer) : void {.importc.}
 #For debugging
 proc print_world*() : void {.importc.}
 
+#For testing purposes outside of SuperCollider's RT allocator, every C function is wrapped in another Nim function that
+#uses the standard allocation when -d:supercollider is not defined.
+
 #RTAlloc wrapper
-proc rt_alloc*(in_size : culong) : pointer {.importc.}
+proc rt_alloc_C*(in_size : culong) : pointer {.importc: "rt_alloc".}
+
+proc rt_alloc*(in_size : culong) : pointer =
+    when defined(supercollider):
+        return rt_alloc_C(in_size)
+    else:
+        return alloc(in_size)
 
 #RTAlloc with 0 memory initialization
-proc rt_alloc0*(in_size : culong) : pointer {.importc.}
+proc rt_alloc0_C*(in_size : culong) : pointer {.importc: "rt_alloc0".}
+
+proc rt_alloc0*(in_size : culong) : pointer =
+    when defined(supercollider):
+        return rt_alloc0_C(in_size)
+    else:
+        return alloc0(in_size)
 
 #RTRealloc
-proc rt_realloc*(in_ptr : pointer, in_size : culong) : pointer {.importc.}
+proc rt_realloc_C*(in_ptr : pointer, in_size : culong) : pointer {.importc: "rt_realloc".}
+
+proc rt_realloc*(in_ptr : pointer, in_size : culong) : pointer =
+    when defined(supercollider):
+        return rt_realloc_C(in_ptr, in_size)
+    else:
+        return realloc(in_ptr, in_size)
 
 #RTFree wrapper
-proc rt_free*(in_ptr : pointer) : void {.importc.}
+proc rt_free_C*(in_ptr : pointer) : void {.importc: "rt_free".}
+
+proc rt_free*(in_ptr : pointer) : void =
+    when defined(supercollider):
+        rt_free_C(in_ptr)
+    else:
+        dealloc(in_ptr)
