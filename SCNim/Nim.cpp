07a7250396a1fca01d89a9724668d8e94d62e68e
Unknown <francesco.cameli95@gmail.com>
2019-10-05 18:17:07 +0100

    Added safety for using libSine.so/dylib without compiling it first

--- SCNim/Nim.cpp
+++ SCNim/Nim.cpp
@@ -13,20 +13,37 @@ static void Nim_Dtor(Nim* unit);
 
 void Nim_Ctor(Nim* unit) 
 {
-    unit->sine_nim_obj = (void*)Nim_UGenConstructor(unit->mInBuf);
+    if(Nim_UGenConstructor)
+        unit->sine_nim_obj = (void*)Nim_UGenConstructor(unit->mInBuf);
+    else
+    {
+        Print("ERROR: No libsine.so/dylib loaded\n");
+        unit->sine_nim_obj = nullptr;
+    }
 
     SETCALC(Nim_next);
+    
     Nim_next(unit, 1);
 }
 
 void Nim_Dtor(Nim* unit) 
 {
-    Nim_UGenDestructor(unit->sine_nim_obj);
+    if(unit->sine_nim_obj)
+        Nim_UGenDestructor(unit->sine_nim_obj);
 }
 
 void Nim_next(Nim* unit, int inNumSamples) 
 {
-    Nim_UGenPerform(unit->sine_nim_obj, inNumSamples, unit->mInBuf, unit->mOutBuf);
+    if(unit->sine_nim_obj)
+        Nim_UGenPerform(unit->sine_nim_obj, inNumSamples, unit->mInBuf, unit->mOutBuf);
+    else
+    {
+        for(int i = 0; i < unit->mNumOutputs; i++)
+        {
+            for(int y = 0; y < inNumSamples; y++)
+                unit->mOutBuf[i][y] = 0.0f;
+        }
+    }
 }
 
 PluginLoad(NimUGens) 
