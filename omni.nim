9398056f7a419d3bca26af09aa2e4fc681e6363f
Unknown <francesco.cameli95@gmail.com>
2019-12-12 17:41:52 +0100

    Keep only SC code in omni.nim (for now)

--- omni.nim
+++ omni.nim
@@ -1,19 +1,28 @@
+#[
 #SuperCollider related code
 when defined(supercollider):
-    import src/sc/sc_alloc/sc_alloc
-    export sc_alloc
+    discard
 
-    import src/sc/sc_data
-    export sc_data
-
-    import src/sc/sc_buffer
-    export sc_buffer
-
+#PD related code
 when defined(puredata):
     discard
 
+#Max related code
 when defined(maxmsp):
     discard
+]#
+
+#This imports here are only SuperCollider related. 
+#As of now, keep it like this in order to have syntax highlighting 
+#on Data and Buffer when doing "import omni".
+import src/sc/sc_alloc/sc_alloc
+export sc_alloc
+
+import src/sc/sc_data
+export sc_data
+
+import src/sc/sc_buffer
+export sc_buffer
 
 #DSL macros
 import src/omni_macros
