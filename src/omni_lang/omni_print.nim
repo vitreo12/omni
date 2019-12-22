67513e5d6bed0383310da1c60d19637344dcbe54
Unknown <francesco.cameli95@gmail.com>
2019-12-22 12:01:05 +0000

    Better printing for int/str/float

--- src/omni_lang/omni_print.nim
+++ src/omni_lang/omni_print.nim
@@ -1,4 +1,12 @@
 #Simply import C's printf as debug
-proc print*(formatstr: cstring) {.importc: "printf", header: "<stdio.h>", varargs, cdecl.}
+proc printf*(formatstr: cstring) : cint {.importc, varargs, header: "<stdio.h>", cdecl.}
 
 #Also have some wrappers for floats, ints, etc...
+proc print*(str : string) : void =
+    discard printf("%s \n", str)
+
+proc print*[T : SomeInteger](val : T) : void =
+    discard printf("%d \n", int(val))
+
+proc print*[T : SomeFloat](val : T) : void =
+    discard printf("%f \n", float(val))
