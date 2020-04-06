463dbe5500af58a797e58eedc66b816eebe2341a
Francesco <francesco.cameli95@gmail.com>
2020-04-06 11:13:35 +0100

    Add MIT license files

--- omni_lang/omni_lang/core/lang/omni_macros.nim
+++ omni_lang/omni_lang/core/lang/omni_macros.nim
@@ -1,3 +1,25 @@
+# MIT License
+# 
+# Copyright (c) 2020 Francesco Cameli
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 #DSL macros
 import omni_io
 import omni_init
@@ -5,7 +27,6 @@ import omni_struct
 import omni_parser
 import omni_def
 import omni_perform
-
 export omni_io, omni_init, omni_parser, omni_def, omni_struct, omni_perform
 
 #Needed for def's arg parsing
@@ -14,5 +35,4 @@ export omni_type_checker
 
 #Utilities
 import omni_types
-
 export omni_types
