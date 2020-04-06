8f0d2bb6538406407d3adbe919690fe260af5cee
Francesco <francesco.cameli95@gmail.com>
2020-04-06 12:10:01 +0100

    remove unused import

--- omni_lang/omni_lang/core/lang/omni_def.nim
+++ omni_lang/omni_lang/core/lang/omni_def.nim
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import macros, omni_type_checker
+import macros
 
 macro def*(function_signature : untyped, code_block : untyped) : untyped =
     var 
