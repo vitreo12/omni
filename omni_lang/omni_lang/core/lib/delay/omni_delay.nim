f2f611a308625ce2c15386268ca195ed32b0a307
Francesco Cameli <francesco.cameli95@gmail.com>
2020-05-20 15:19:24 +0100

    Delay only supports signal/float

--- omni_lang/omni_lang/core/lib/delay/omni_delay.nim
+++ omni_lang/omni_lang/core/lib/delay/omni_delay.nim
@@ -21,35 +21,32 @@
 # SOFTWARE.
 
 import ../../lang/omni_call_types
+import ../../lang/omni_types
 import ../alloc/omni_alloc
 import ../data/omni_data
 import ../auto_mem/omni_auto_mem
 import ../math/omni_math
 
 type
-    Delay_struct_inner*[T] = object
+    Delay_struct_inner* = object
         mask  : int
         phase : int
-        data  : Data[T]
+        data  : Data[signal]
 
-    Delay*[T] = ptr Delay_struct_inner[T]
+    Delay* = ptr Delay_struct_inner
 
-proc struct_new_inner*[S : SomeNumber](obj_type : typedesc[Delay], size : S = uint(1), dataType : typedesc = typedesc[float], ugen_auto_mem : ptr OmniAutoMem, ugen_call_type : typedesc[CallType] = InitCall) : Delay[dataType] {.inline.} =
-    #Trying to allocate in perform block! nonono
+proc struct_new_inner*[S : SomeNumber](obj_type : typedesc[Delay], size : S = uint(1), ugen_auto_mem : ptr OmniAutoMem, ugen_call_type : typedesc[CallType] = InitCall) : Delay {.inline.} =
+    #Trying to allocate in perform block!
     when ugen_call_type is PerformCall:
         {.fatal: "attempting to allocate memory in the 'perform' or 'sample' blocks for 'struct Delay'".}
 
-    #error out if trying to instantiate any dataType that is not a Number
-    when dataType isnot SomeNumber: 
-        {.fatal: "Delay's dataType must be some number type".}
-
     #Allocate obj
-    result = cast[Delay[dataType]](omni_alloc(culong(sizeof(Delay_struct_inner[dataType]))))
+    result = cast[Delay](omni_alloc(culong(sizeof(Delay_struct_inner))))
 
     #Allocate data
     let 
         delay_length = int(nextPowerOfTwo(int(size)))
-        data  = Data.struct_new_inner(delay_length, dataType=dataType, ugen_auto_mem=ugen_auto_mem, ugen_call_type=ugen_call_type)
+        data  = Data.struct_new_inner(delay_length, dataType=signal, ugen_auto_mem=ugen_auto_mem, ugen_call_type=ugen_call_type)
         mask  = int(delay_length - 1)
 
     #Register obj (data has already been registered in Data.struct_new_inner)
@@ -60,17 +57,17 @@ proc struct_new_inner*[S : SomeNumber](obj_type : typedesc[Delay], size : S = ui
     result.phase = 0
     result.data = data
 
-template struct_new*[S : SomeNumber](obj_type : typedesc[Delay], size : S = uint(1), dataType : typedesc = typedesc[float]) : untyped {.dirty.} =
-    struct_new_inner(Delay, size, dataType, ugen_auto_mem, ugen_call_type)
+template struct_new*[S : SomeNumber](obj_type : typedesc[Delay], size : S = uint(1)) : untyped {.dirty.} =
+    struct_new_inner(Delay, size, ugen_auto_mem, ugen_call_type)
 
-template new*[S : SomeNumber](obj_type : typedesc[Delay], size : S = uint(1), dataType : typedesc = typedesc[float]) : untyped {.dirty.} =
-    struct_new_inner(Delay, size, dataType, ugen_auto_mem, ugen_call_type)
+template new*[S : SomeNumber](obj_type : typedesc[Delay], size : S = uint(1)) : untyped {.dirty.} =
+    struct_new_inner(Delay, size, ugen_auto_mem, ugen_call_type)
 
 proc checkValidity*(obj : Delay, ugen_auto_buffer : ptr OmniAutoMem) : bool =
     return true
 
 #Read proc (uses cubic interp)
-proc read*[T : SomeNumber, Y : SomeNumber](delay : Delay[T], delay_time : Y) : T {.inline.} =
+proc read*[Y : SomeNumber](delay : Delay, delay_time : Y) : float {.inline.} =
     let 
         float_delay_time = float(delay_time)
         frac : float = float_delay_time - delay_time
@@ -80,9 +77,9 @@ proc read*[T : SomeNumber, Y : SomeNumber](delay : Delay[T], delay_time : Y) : T
         index3 = (index + 2) and delay.mask
         index4 = (index + 3) and delay.mask
 
-    return cubic_interp(frac, delay.data[index1], delay.data[index2], delay.data[index3], delay.data[index4])
+    return float(cubic_interp(frac, delay.data[index1], delay.data[index2], delay.data[index3], delay.data[index4]))
 
 #Write proc
-proc write*[T : SomeNumber, Y : SomeNumber](delay : Delay[T], val : Y) : void {.inline.} =
-    delay.data[delay.phase] = T(val)
+proc write*[Y : SomeNumber](delay : Delay, val : Y) : void {.inline.} =
+    delay.data[delay.phase] = float(val)
     delay.phase = (delay.phase + 1) and delay.mask
