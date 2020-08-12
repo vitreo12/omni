aa71a28a61b17e64d204e48c572e451f723b8747
Francesco Cameli <francesco.cameli95@gmail.com>
2020-08-12 16:18:42 +0100

    weird recursive call in test47

--- omni_lang/omni_lang/tests/test47.nim
+++ omni_lang/omni_lang/tests/test47.nim
@@ -3,8 +3,11 @@ import ../../omni_lang, macros
 ins 1
 
 expandMacros:
+    def a[T](x T = 0.0) T:
+        return a(x) + 1
+
     def process():
-        sample_out = 0
+        sample_out = a(1.23) * a(1)
         sample_out += 1.0
         #result = sample_out
         #return Data(10)
