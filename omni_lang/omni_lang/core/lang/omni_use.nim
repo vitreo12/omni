2093972f00875d144180baaaff2d7ae2de3b2b36
Francesco Cameli <francesco.cameli95@gmail.com>
2020-09-21 14:06:56 +0100

    require -> use

--- omni_lang/omni_lang/core/lang/omni_use.nim
+++ omni_lang/omni_lang/core/lang/omni_use.nim
@@ -22,56 +22,6 @@
 
 import macros, os
 
-#This could perhaps support untyped in the future
-
-#This is what require does: 
-#import "ImportMe.nim" as ImportMe_module
-#Otherwise, module names will cluster the namespace if they have same name as structs, or defs
-
-proc check_valid_path(path : NimNode, unified_path_list : var seq[NimNode], as_names : var seq[NimNode]) : void {.compileTime.} =
-    let path_kind = path.kind
-    
-    if path_kind != nnkStrLit and path_kind != nnkIdent and path_kind != nnkInfix:
-        error("require: invalid syntax. Only strings or identifiers are valid to set the paths.")
-    
-    #Custom as
-    if path_kind == nnkInfix:
-        let 
-            infix_name = path[0].strVal()
-            path_name  = path[1]
-            module_as_name = path[2]
-
-        if infix_name != "as":
-            error("require: invalid infix '" & $infix_name & "'")
-
-        if path_name.kind != nnkStrLit and path.kind != nnkIdent:
-            error("require: invalid syntax. Only strings or identifiers are valid to set the paths.")
-
-        if module_as_name.kind != nnkIdent:
-            error("require: invalid module name")
-        
-        let module_as_name_module_inner = newIdentNode(module_as_name.strVal() & "_module_inner")
-
-        unified_path_list.add(path_name)
-        as_names.add(module_as_name_module_inner)
-
-    #String or ident
-    else:
-        let path_without_extension = (path.strVal().splitFile().name)
-        unified_path_list.add(path)
-        as_names.add(newIdentNode(path_without_extension & "_module_inner"))
-
-proc check_valid_paths(path_list : NimNode, paths_same_line : NimNode, unified_path_list : var seq[NimNode], as_names : var seq[NimNode]) : void {.compileTime.} =
-    if path_list.len == 0:
-        check_valid_path(path_list, unified_path_list, as_names)
-    else:
-        for path in path_list:
-            check_valid_path(path, unified_path_list, as_names)
-    
-    #varargs: require "One.omni", "Two.omni"
-    for path in paths_same_line:
-        check_valid_path(path, unified_path_list, as_names)
-
 #use "Path.omni":
     #Something as Something1 
     #someFunc as someFunc1
@@ -96,4 +46,4 @@ macro use*(path : untyped, ident_list : untyped = nil) : untyped =
     for line in ident_list:
         echo astGenRepr line
 
-    error "ye"
+    error repr result
