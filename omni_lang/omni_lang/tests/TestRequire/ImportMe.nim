eb9da701510e52db6191d99aa5c0976dd63d7b12
Francesco Cameli <francesco.cameli95@gmail.com>
2020-09-25 17:53:24 +0100

    temp solution

--- omni_lang/omni_lang/tests/TestRequire/ImportMe.nim
+++ omni_lang/omni_lang/tests/TestRequire/ImportMe.nim
@@ -4,9 +4,9 @@ import ../../../omni_lang, macros
     ImportMeImportMe as ImportMeImportMe1
     something as something2 ]#
 
-#expandMacros:
-struct ImportMe[T]:
-    a T
+expandMacros:
+    struct ImportMe[T]:
+        a T
 
 #[ def something(a ImportMe):
     print("something - ImportMe")
@@ -39,6 +39,14 @@ expandMacros:
 def blah():
     print("blah") ]#
 
+proc ImportMe_struct_new_inner_test(T : typedesc = typedesc[float], obj_type: typedesc[ImportMe_struct_export[T]], a: T = 0, ugen_auto_mem: ptr OmniAutoMem, ugen_call_type: typedesc[CallType] = InitCall): ImportMe[T] {.inline.} =
+    when ugen_call_type is PerformCall:
+        {.fatal: "attempting to allocate memory in the \'perform\' or \'sample\' blocks for \'struct ImportMe\'".}
+    result = cast[ImportMe[T]](omni_alloc(culong(sizeof(ImportMe_struct_inner[T]))))
+    registerChild(ugen_auto_mem, result)
+    result.a = a
+
 init:
-    a = ImportMe()
+    #a = ImportMe()
+    a = ImportMe_struct_new_inner_test(typedesc[float], ImportMe_struct_export, 0, ugen_auto_mem, ugen_call_type)
     a.blah()
