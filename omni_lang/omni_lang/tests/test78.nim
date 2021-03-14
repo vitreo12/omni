88fd65e3e88f7efc47a969ba5168bde52c5e6edc
Francesco Cameli <francesco.cameli95@gmail.com>
2021-03-14 20:07:34 +0000

    fix typos

--- omni_lang/omni_lang/tests/test78.nim
+++ omni_lang/omni_lang/tests/test78.nim
@@ -5,7 +5,6 @@ struct Something[T]:
 def newData[T]():
   return Data[T](samplerate)
 
-
 omni_debug_macros:
   struct SomethingElse[T]:
     a = 0.5
@@ -14,8 +13,8 @@ omni_debug_macros:
     something2 = Something[T](samplerate) #using a constructor: type is inferred
     data Data[T] = newData[T]() #not calling a constructor: must be explicit on the type!
 
-init:
-  a = SomethingElse()
-  b = SomethingElse[int]()
+  init:
+    a = SomethingElse()
+    b = SomethingElse[int]()
 
-sample: discard
+  sample: discard
