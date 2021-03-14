710c7ea9a392dc68aa190b5d3111353f61f057d8
Francesco Cameli <francesco.cameli95@gmail.com>
2021-03-14 17:25:43 +0000

    WIP

--- omni_lang/omni_lang/tests/test79.nim
+++ omni_lang/omni_lang/tests/test79.nim
@@ -1,14 +1,20 @@
-# omni_debug_macros:
-def someFunc[T](a T):
-  return a 
+omni_debug_macros:
+  def someFunc[T](a T):
+    return a 
 
-sample:
-  out1 = someFunc[int](10)
+  sample:
+    out1 = someFunc[int](0)
 
-# proc someFunc_inner[T : SomeNumber]() : auto =
-#   return T(0.5)
+proc someFunc_inner[T : SomeNumber]() : auto =
+  return T(0.5)
 
 # template someFunc(G1 : typedesc = typedesc[float]) : untyped =
 #   someFunc_inner[G1]()
 
 # echo someFunc(int)
+
+
+# proc test(T : typedesc[SomeNumber] = float, b : T) =
+#     return b * 2
+
+# echo a(b = 0.5)
