cdac9556a9215e28ee4a8abfe3929ead16651af0
Francesco Cameli <francesco.cameli95@gmail.com>
2021-03-15 18:27:59 +0000

    auto_mem: fix "not" return

--- omni_lang/omni_lang/core/lang/omni_init.nim
+++ omni_lang/omni_lang/core/lang/omni_init.nim
@@ -416,7 +416,7 @@ macro omni_init_inner*(code_block_stmt_list : untyped) : untyped =
             omni_check_datas_validity(omni_ugen, samplerate, bufsize, omni_auto_mem, omni_call_type)
             
             #check omni_auto_mem's alloc for validity, else false. This happens if any allocation of structs failed
-            if omni_check_auto_mem_validity(omni_auto_mem):
+            if not omni_check_auto_mem_validity(omni_auto_mem):
                 Omni_UGenFree(omni_ugen_ptr)
                 return false
 
