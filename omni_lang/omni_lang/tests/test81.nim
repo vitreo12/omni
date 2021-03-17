b583cce4a04a8655ed72abeed3ec4053d68a93cf
Francesco Cameli <francesco.cameli95@gmail.com>
2021-03-17 23:22:30 +0000

    struct: allow all constructor syntaxes to work with constructor definitions

--- omni_lang/omni_lang/tests/test81.nim
+++ omni_lang/omni_lang/tests/test81.nim
@@ -1,17 +1,17 @@
-# omni_debug_macros:
-struct Something:
-  a = Data.new
-  b = Data[float].new
-  c = new Data
-  d = new Data[float]
-  e = Data.new(100)
-  f = Data[float].new(100)
-  g = new Data(100)
-  h = new Data[float](100)
-  i = Data(100)
-  l = Data[float](100)
+omni_debug_macros:
+  struct Something:
+    a = Data.new
+    b = Data[float].new
+    c = new Data
+    d = new Data[float]
+    e = Data.new(100)
+    f = Data[float].new(100)
+    g = new Data(100)
+    h = new Data[float](100)
+    i = Data(100)
+    l = Data[float](100)
 
-init:
-  a = Something(g = Data())
+  init:
+    a = Something()
 
-sample: discard
+  sample: discard
